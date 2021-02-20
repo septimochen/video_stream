585c0a05bc7032d7f50363b4166147f3cdd1be3e
septimochen <qizu.chen@hotmail.com>
2021-02-20 22:51:42 +0800

    update

--- src/schemas.py
+++ src/schemas.py
@@ -3,7 +3,19 @@ from typing import List, Optional
 from pydantic import BaseModel
 from sqlalchemy import orm
 
+class ActorBase(BaseModel):
+    name: str
+    avatar: str
 
+class ActorCreate(ActorBase):
+    pass
+
+class Actor(ActorBase):
+    id: int
+    # vods: List[Vod] = []
+
+    class Config:
+        orm_mode = True
 class VodBase(BaseModel):
     title: str
     thumbnail: str
@@ -14,23 +26,11 @@ class VodCreate(VodBase):
 
 class Vod(VodBase):
     id: int
-    actor_id: int
+    actor: Actor
 
     class Config:
         orm_mode = True
 
 
-class ActorBase(BaseModel):
-    name: str
-    avatar: str
 
-class ActorCreate(ActorBase):
-    pass
-
-class Actor(ActorBase):
-    id: int
-    vods: List[Vod] = []
-
-    class Config:
-        orm_mode = True
 
